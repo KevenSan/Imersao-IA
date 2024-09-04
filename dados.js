aaa374ff7a96100ee2872d48a58ab937f97fa504
KevenSan <kevensan013@outlook.com>
2024-09-04 17:34:28 -0300

    Aula3 Javascript

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+     {
+        tituloNome: "Neymar Junior",
+        descricao: "Neymar da Silva Santos Júnior, " +
+                   "mais conhecido como Neymar, é um dos maiores jogadores de futebol da atualidade. " +
+                   "Com um estilo de jogo ágil, dribles desconcertantes e um talento nato para marcar gols, " +
+                   "Neymar conquistou fãs em todo o mundo. " +
+                   "Sua carreira começou nas categorias de base do Santos, onde logo se destacou.",
+        link: "https://pt.wikipedia.org/wiki/Neymar#:~:text=Neymar%20da%20Silva%20Santos%20J%C3%BAnior,%2DHilal%2C%20da%20Ar%C3%A1bia%20Saudita.&text=clubes%2C%20atualizadas%20at%C3%A9%203%20de%20outubro%20de%202023.&text=at%C3%A9%2017%20de%20outubro%20de%202023.",
+    },
+    
+    {
+        tituloNome : "Pedro",
+        descricao : "Pedro Guilherme Abreu dos Santos,"+ "conhecido como Pedro, é um atacante brasileiro+ que  se destaca"+
+        "pelo Flamengo. Conhecido por sua finalização" +"precisa e pelo seu jogo aéreo, Pedro é um dos"+
+        "principais artilheiros do futebol brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Pedro_Guilherme_Abreu_dos_Santos",
+    
+    },
+    
+   {
+        tituloNome : "Messi",
+        descricao : "Lionel Andrés Messi, conhecido como"+ "Messi, é considerado um dos maiores jogadores de"+ "futebol de"+
+        "todos os tempos. Com uma habilidade única para"+ "driblar e marcar gols, Messi conquistou diversos"+
+        "títulos pelo Barcelona e pela seleção argentina.",
+        link : "https://pt.wikipedia.org/wiki/Lionel_Messi",
+    }
+
+];
+
+// let jogadorNeymar = {
+//     tituloNome: "Neymar Junior",
+//     descricao: "Neymar da Silva Santos Júnior, " +
+//                "mais conhecido como Neymar, é um dos maiores jogadores de futebol da atualidade. " +
+//                "Com um estilo de jogo ágil, dribles desconcertantes e um talento nato para marcar gols, " +
+//                "Neymar conquistou fãs em todo o mundo. " +
+//                "Sua carreira começou nas categorias de base do Santos, onde logo se destacou.",
+//     link: "https://pt.wikipedia.org/wiki/Neymar#:~:text=Neymar%20da%20Silva%20Santos%20J%C3%BAnior,%2DHilal%2C%20da%20Ar%C3%A1bia%20Saudita.&text=clubes%2C%20atualizadas%20at%C3%A9%203%20de%20outubro%20de%202023.&text=at%C3%A9%2017%20de%20outubro%20de%202023.",
+// };
+
+// let jogadorPedro = {
+//     tituloNome : "Pedro",
+//     descricao : "Pedro Guilherme Abreu dos Santos,"+ "conhecido como Pedro, é um atacante brasileiro+ que  se destaca"+
+//     "pelo Flamengo. Conhecido por sua finalização" +"precisa e pelo seu jogo aéreo, Pedro é um dos"+
+//     "principais artilheiros do futebol brasileiro.",
+//     link : "https://pt.wikipedia.org/wiki/Pedro_Guilherme_Abreu_dos_Santos",
+
+// };
+
+// let jogadorMessi ={
+//     tituloNome : "Messi",
+//     descricao : "Lionel Andrés Messi, conhecido como"+ "Messi, é considerado um dos maiores jogadores de"+ "futebol de"+
+//     "todos os tempos. Com uma habilidade única para"+ "driblar e marcar gols, Messi conquistou diversos"+
+//     "títulos pelo Barcelona e pela seleção argentina.",
+//     link : "https://pt.wikipedia.org/wiki/Lionel_Messi",
+// }
+//dados.push("Neymar Junior, Pedro, Messi");
